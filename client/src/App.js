f90bb55dc98d982b9ffaeb25ee1c491a33e77fda
Jared Silver <jrsilver88@gmail.com>
2023-01-10 23:18:57 -0800

    skeleton setup, working on register/login page using react context

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Dashboard, Register, Landing, Error } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,34 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Jobify is a powerful job tracking app that helps job seekers and
+            professionals stay organized and on top of their job search. With
+            Jobify, you can easily track the jobs you've applied to, the status
+            of your applications, and any upcoming interviews or events related
+            to your job search.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
